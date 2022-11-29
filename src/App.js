3c5a47909328c679bb04ad3eb6d4cb028143cd3b
yetemegn-telaye <lemma19yet@gmail.com>
2022-11-30 00:52:41 +0300

    Add route for rockets component and use it as navlink

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import {
 } from 'react-router-dom';
 import Header from './components/Header';
 import Myprofile from './components/Myprofile';
+import Rockets from './components/Rockets/Rockets';
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
+        <Route path="/" element={<Rockets />} />
         <Route path="/myProfile" element={<Myprofile />} />
       </Routes>
     </Router>
