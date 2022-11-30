a80e83eea256e23c9411c52079a6f8bad00c5647
yetemegn-telaye <lemma19yet@gmail.com>
2022-11-30 16:16:25 +0300

    Fetch rockets data from api

--- src/redux/rockets.js
+++ src/redux/rockets.js
@@ -1,7 +1,42 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const rocketsUrl = 'https://api.spacexdata.com/v3/rockets/';
+const rocketsList = [
+  {
+    id: '0',
+    rocket_name: 'Initial Rocket',
+    description: 'Initial rocket description.',
+    flickr_images: 'https://imgur.com/azYafd8.jpg',
+  },
+];
+
+export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async () => {
+  const resp = await axios.get(rocketsUrl);
+  if (resp.data) {
+    return resp.data;
+  }
+  return [];
+});
 
 const rocketSlice = createSlice({
-
+  name: 'rockets',
+  initialState: rocketsList,
+  reducers: {},
+  extraReducers: (build) => {
+    build.addCase(fetchRockets.fulfilled, (state, action) => {
+      const newState = state;
+      Object.entries(action.payload).forEach((elt) => {
+        newState.push({
+          id: elt[1].rocket_id,
+          rocket_name: elt[1].rocket_name,
+          description: elt[1].description,
+          flickr_images: elt[1].flickr_images[0],
+        });
+      });
+      return newState;
+    });
+  },
 });
 
 export default rocketSlice.reducer;
