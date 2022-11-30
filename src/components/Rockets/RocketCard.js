d5fa554ed5f7b849b24f447cc5e86b92631c9e10
yetemegn-telaye <lemma19yet@gmail.com>
2022-11-30 16:20:06 +0300

    Display the fetched rockets data in rockets component and styled it

--- src/components/Rockets/RocketCard.js
+++ src/components/Rockets/RocketCard.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import '../../styles/rockets.css';
+import PropTypes from 'prop-types';
 
-const RocketCard = () => (
-  <div className="rocket-card">
-    <div className="rocket-img">
-      <img alt="rocket pic" />
-      Image
+const RocketCard = (props) => {
+  const {
+    rocketName, description, rocketImg,
+  } = props;
+  return (
+    <div className="rocket-card">
+
+      <img src={rocketImg} className="rocket-img" alt="rocket pic" />
+
+      <div className="rocket-desc">
+        <h3 className="rocket-name">
+          {rocketName}
+        </h3>
+        <p>
+          {description}
+        </p>
+        <button type="button" className="reserve-btn">Reserve Rocket</button>
+      </div>
     </div>
-    <div className="rocket-desc">
-      <h3 className="rocket-name">Falcon 1</h3>
-      <p>
-        The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX
-        during 2006-2009. On 28 September 2008, Falcon 1 became the first
-        privately-developed liquid-fuel
-        launch vehicle to go into orbit around the Earth.
-      </p>
-      <button type="button" className="reserve-btn">Reserve Rocket</button>
-    </div>
-  </div>
-);
+  );
+};
+RocketCard.propTypes = {
+  rocketName: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  rocketImg: PropTypes.string.isRequired,
+};
 export default RocketCard;
