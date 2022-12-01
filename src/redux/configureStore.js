cd57d6d63fc3a5423d68ba47bddaf88755cdc4b9
blase147 <solarmails2@gmail.com>
2022-12-02 00:18:17 +0100

    Fetch mission data

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketsReducer from './rockets';
+import missionsReducer from './missions';
 
 const store = configureStore({
   reducer: {
     rockets: rocketsReducer,
+    missions: missionsReducer,
   },
 });
 export default store;
