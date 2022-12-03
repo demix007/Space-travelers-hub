72e16670b3b9fd53724be5d674556eea4ff63450
blase147 <solarmails2@gmail.com>
2022-12-03 19:46:52 +0100

    Apply testing

--- src/test/missions.test.js
+++ src/test/missions.test.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import {render} from '@testing-library/react';
-import { Provider } from "react-redux";
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import store from '../redux/configureStore';
-import Mission from '../components/Missions/Mission'
-import Missions from "../components/Missions/Missions";
+import Mission from '../components/Missions/Mission';
+import Missions from '../components/Missions/Missions';
 
-describe('Missions',()=>{
-    it('Mission component is rendered correctly',()=>{
-        const mission = render(
-            <Provider store={store}>
-                <Mission/>
-            </Provider>
-        );
-        expect(mission).toMatchSnapshot();
-    });
+describe('Missions', () => {
+  it('Mission component is rendered correctly', () => {
+    const mission = render(
+      <Provider store={store}>
+        <Mission />
+      </Provider>,
+    );
+    expect(mission).toMatchSnapshot();
+  });
 
-   it('Rockets component rendered correctly',()=>{
+  it('Rockets component rendered correctly', () => {
     const missions = render(
-        <Provider store={store}>
-            <Missions/>
-        </Provider>
+      <Provider store={store}>
+        <Missions />
+      </Provider>,
     );
     expect(missions).toMatchSnapshot();
-   });
-})
+  });
+});
